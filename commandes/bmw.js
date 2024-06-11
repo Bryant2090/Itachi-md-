81e8aed38fbbdf396c8433f252b7899513d092f6
Xtech <165713304+Bryant2090@users.noreply.github.com>
2024-06-11 08:31:18 +0000

    Update bmw.js

--- commandes/bmw.js
+++ commandes/bmw.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
-france({ nomCom: "Dragon", reaction: "😏", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+france({ nomCom: "Itachi", reaction: "😏", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = '𝒀𝒆𝒔😉 𝐚𝐦 ITACHI MD 𝐜𝐫𝐞𝐚𝐭𝐞𝐝 𝐛𝐲 BRYANT TECH\n\n ' + "𝐈𝐟 𝐲𝐨𝐮 𝐰𝐚𝐧𝐧𝐚 𝐮𝐬𝐞 𝐦𝐞 𝐣𝐮𝐬𝐭 𝐭𝐲𝐩𝐞 𝐦𝐞𝐧𝐮 𝐭𝐨 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 𝐦𝐞";
     let d = '                                                ❤️‍🔥𝐎𝐟 𝐜𝐨𝐮𝐫𝐬𝐞 𝐚𝐦 𝐭𝐡𝐞 𝐛𝐞𝐬𝐭 𝐛𝐨𝐭 𝐢𝐧 𝐭𝐡𝐞 𝐮𝐧𝐢𝐯𝐞𝐫𝐬𝐞';
