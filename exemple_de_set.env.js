bd5c83198045175d28d1c71bf93f0002c822e086
King Bryant <165713304+Bryant2090@users.noreply.github.com>
2024-06-10 14:23:09 -1200

    Add files via upload

---
+++ exemple_de_set.env.js
@@ -0,0 +1,14 @@
+OWNER_NAME="𝐊𝐞𝐢𝐭𝐡 𝐊𝐞𝐢𝐳𝐳𝐚𝐡"
+PREFIX="."
+PUBLIC_MODE='yes'
+AUTO_READ_STATUS="yes"
+AUTO_DOWNLOAD_STATUS="no"
+BOT_NAME= "𝐇𝐔𝐍𝐂𝐇𝐎-𝐌𝐃"
+IMAGE_MENU= "https://telegra.ph/file/835c3a335c8e2585898fc.jpg"
+NUMERO_OWNER= ""
+OWNER_NAME= "᚛Keith Keizzah᚜"
+DATABASE_URL= ""
+WARN_COUNT= ""
+OPENAI_API_KEY= ""
+STARTING_BOT_MESSAGE='yes'
+ANTI_DELETE_MESSAGE='yes'
